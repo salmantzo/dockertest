9fc76225b35ce50635737196c60662f350c9dd71
Matthew Rayner <matthew@timezoneone.com>
2021-08-05 11:32:08 +1200

    Fix project env values not overriding default env values

--- start.sh
+++ start.sh
@@ -8,7 +8,7 @@ PROJECT_ENV_FILE=../$COMPOSE_PROJECT_NAME/docker/.env
 
 if [ -f "$PROJECT_ENV_FILE" ]; then
     # Merge docker and project env files together
-    sort -u -t '=' -k 1,1 ./.env $PROJECT_ENV_FILE | grep -v '^$\|^\s*\#' > ./config/envs/$COMPOSE_PROJECT_NAME.env
+    sort -u -t '=' -k 1,1 $PROJECT_ENV_FILE ./.env | grep -v '^$\|^\s*\#' > ./config/envs/$COMPOSE_PROJECT_NAME.env
 else
     sort -u -t '=' -k 1,1 ./.env | grep -v '^$\|^\s*\#' > ./config/envs/$COMPOSE_PROJECT_NAME.env
 fi
