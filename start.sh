d3bf508ac8b923a56b73fc57726fa95e909726aa
Ed Millington <ed@timezoneone.com>
2021-10-22 09:03:46 +1300

    Check for WordPress .env file

--- start.sh
+++ start.sh
@@ -18,11 +18,20 @@ if [ ! -d "$PROJECT_FOLDER" ]; then
     exit 1
 fi
 
-PROJECT_ENV_FILE=../$COMPOSE_PROJECT_NAME/docker/.env
+SS_PROJECT_ENV_FILE=../$COMPOSE_PROJECT_NAME/docker/.env
+WP_PROJECT_ENV_FILE=../$COMPOSE_PROJECT_NAME/wp-content/themes/$COMPOSE_PROJECT_NAME/docker/.env
+
+PROJECT_ENV_FILE=$SS_PROJECT_ENV_FILE
 
 if [ ! -f "$PROJECT_ENV_FILE" ]; then
-    echo "ERROR: .env file not found in project directory"
-    exit 1
+
+    PROJECT_ENV_FILE=$WP_PROJECT_ENV_FILE
+
+    if [ ! -f "$PROJECT_ENV_FILE" ]; then
+
+        echo "ERROR: .env file not found in project directory"
+        exit 1
+    fi
 fi
 
 # Check if /docker/.env exists in project folder
